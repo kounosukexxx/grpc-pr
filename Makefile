d455d65725ffafb9b99d02778fdbccd179627df2
shota-aa <dqx1005@icloud.com>
2022-03-24 17:33:20 +0900

    make GetUser

--- Makefile
+++ Makefile
@@ -1,4 +1,19 @@
-PHONY: proto
-proto: 
-	protoc --proto_path=proto/. --go-grpc_opt require_unimplemented_servers=false,paths=source_relative --go-grpc_out pb/ --go_opt paths=source_relative --go_out pb/ proto/*.proto
+.PHONY: protobuf
+protobuf: protobuf-go protobuf-doc
 
+.PHONY: protobuf-go
+protobuf-go:
+	rm -rf pb
+	mkdir -p pb
+	protoc --proto_path=proto/. --go-grpc_opt require_unimplemented_servers=false,paths=source_relative --go-grpc_out pb/ --go_opt paths=source_relative --go_out pb/ proto/**/*.proto
+
+.PHONY: protobuf-doc
+protobuf-doc:
+	rm -rf docs/protobuf_schema
+	mkdir -p docs/protobuf_schema
+	protoc --doc_out=html,rest.html:docs/protobuf_schema proto/rest/*.proto
+
+
+.PHONY: evans
+evans:
+	evans -r repl --port 8080
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net"
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+	user "github.com/shota-aa/grpc-pr/pb/rest"
+	"google.golang.org/grpc/reflection"
+)
 
 func main() {
-	fmt.Println("")
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	user.RegisterUserServiceServer(s, &server{})
+	reflection.Register(s)
+	log.Printf("Listening on %v", ":8080")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+type server struct{}
+
+func (s *server) GetUser(ctx context.Context, req *user.GetUserRequest) (*user.User, error) {
+	log.Printf("Received: %v", req.Id)
+	now, _ := ptypes.TimestampProto(time.Now())
+	return &user.User{Id: req.Id, Name: "John Smith", Email: "johnsmith@example.com", UpdatedAt: now}, nil
 }
