825d8800ff39316fd5b63566ae04c7d393fd3672
shota-aa <dqx1005@icloud.com>
2022-03-28 17:57:55 +0900

    for pubsub

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	cloud.google.com/go/compute v1.5.0 // indirect
 	cloud.google.com/go/firestore v1.6.1 // indirect
 	cloud.google.com/go/iam v0.3.0 // indirect
+	cloud.google.com/go/pubsub v1.19.0 // indirect
 	cloud.google.com/go/storage v1.10.0 // indirect
 	firebase.google.com/go v3.13.0+incompatible // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
@@ -27,6 +28,7 @@ require (
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/oauth2 v0.0.0-20220309155454-6242fa91716a // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
 	"os"
+	"strconv"
+	"sync"
 	"time"
 
+	"cloud.google.com/go/pubsub"
 	firebase "firebase.google.com/go"
 	"github.com/golang/protobuf/ptypes"
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
@@ -15,10 +19,11 @@ import (
 	user "github.com/shota-aa/grpc-pr/pb"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"google.golang.org/api/option"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func main() {
@@ -33,18 +38,20 @@ func main() {
 
 	ctx := context.Background()
 	projectID := "kauche-practice"
+
+	// firestore
 	conf := &firebase.Config{ProjectID: projectID}
-	sa := option.WithCredentialsFile("credential/serviceAccount.json")
-	app, err := firebase.NewApp(ctx, conf, sa)
+	// sa := option.WithCredentialsFile("credential/serviceAccount.json")
+	app, err := firebase.NewApp(ctx, conf)
 	// app, err := firebase.NewApp(ctx, conf)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	client, err := app.Firestore(ctx)
+	firestoreClient, err := app.Firestore(ctx)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	defer client.Close()
+	defer firestoreClient.Close()
 	log.Println("aaaaaaaaaaaa")
 
 	// client, err := firestore.NewClient(ctx, "kauche-practice")
@@ -53,8 +60,8 @@ func main() {
 	// 	log.Fatal(err)
 	// }
 
-	log.Print("aa")
-	doc, _, err := client.Collection("users").Add(ctx, map[string]interface{}{
+	// log.Print("aa")
+	doc, _, err := firestoreClient.Collection("users").Add(ctx, map[string]interface{}{
 		"name": "hello1",
 	})
 	log.Print("ww")
@@ -85,6 +92,106 @@ func main() {
 	// 	fmt.Printf("%v\n", doc.Data())
 	// }
 
+	// pubsub
+	pubsubClient, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	defer pubsubClient.Close()
+
+	// publisher
+	// topicID, _ := random.MakeRandomStr(10)
+	topicID1 := "test1"
+	topic1 := pubsubClient.Topic(topicID1)
+	t := 0
+	for t < 12 {
+		t++
+		result := topic1.Publish(ctx, &pubsub.Message{
+			Data: []byte("Hello World" + strconv.Itoa(t)),
+		})
+		id, err := result.Get(ctx)
+		if err != nil {
+			fmt.Println(err)
+		}
+		// topic, err := pubsubClient.CreateTopic(topicID)
+		// if err != nil {
+		// 	log.Fatalf("Failed to create topic: %v", err)
+		// }
+		fmt.Printf("Topic %s got.\n", topic1)
+		fmt.Printf("Published a message; msg ID: %v\n", id)
+	}
+
+	pbuser := &user.User{
+		Id:        999,
+		Name:      "kounosuke",
+		Email:     "dqx@com",
+		UpdatedAt: timestamppb.Now(),
+	}
+	topicID2 := "test2-proto"
+	topic2 := pubsubClient.Topic(topicID2)
+	// cfg, err := topic2.Config(ctx)
+	// if err != nil {
+	// 	fmt.Printf("topic.Config err: %v", err)
+	// }
+	// cfg := &pubsub.TopicConfig{
+	// 	SchemaSettings: &pubsub.SchemaSettings{
+	// 		Schema:   fmt.Sprintf("projects/%s/schemas/%s", projectID, "aaa"),
+	// 		Encoding: pubsub.EncodingBinary,
+	// 	},
+	// }
+	// encoding := cfg.SchemaSettings.Encoding
+	// fmt.Println(encoding)
+	// var msg2 []byte
+	// switch encoding {
+	// case pubsub.EncodingBinary:
+	// 	msg2, err = proto.Marshal(pbuser)
+	// 	if err != nil {
+	// 		fmt.Printf("proto.Marshal err: %v", err)
+	// 	}
+	// case pubsub.EncodingJSON:
+	// 	msg2, err = protojson.Marshal(pbuser)
+	// 	if err != nil {
+	// 		fmt.Printf("protojson.Marshal err: %v", err)
+	// 	}
+	// default:
+	// 	fmt.Printf("invalid encoding: %v", encoding)
+	// }
+	msg2, err := proto.Marshal(pbuser)
+	if err != nil {
+		fmt.Printf("proto.Marshal err: %v", err)
+	}
+	result2 := topic2.Publish(ctx, &pubsub.Message{
+		Data: msg2,
+	})
+	_, err = result2.Get(ctx)
+	if err != nil {
+		fmt.Printf("result.Get: %v", err)
+	}
+	a := user.User{}
+	err = proto.Unmarshal(msg2, &a)
+	fmt.Printf("msg2: %v\n", a)
+
+	// subscriber
+	var mu sync.Mutex
+	received := 0
+	subID := "test1-sub"
+	sub := pubsubClient.Subscription(subID)
+	cctx, cancel := context.WithCancel(ctx)
+	err = sub.Receive(cctx, func(ctx context.Context, msg *pubsub.Message) {
+		fmt.Println("1")
+		msg.Ack()
+		fmt.Printf("Got message: %q\n", string(msg.Data))
+		mu.Lock()
+		defer mu.Unlock()
+		received++
+		fmt.Println("2")
+		if received == 10 {
+			cancel()
+		}
+	})
+	if err != nil {
+		fmt.Print(err)
+	}
 	// logging
 	zap, err := zap.NewProduction()
 	if err != nil {
